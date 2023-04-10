157d152efc75edcee79c7bb48c09209261bdbd34
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-04-09 17:46:41 -0700

    Obj lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    address : "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+const newObj = { ...obj};
+newObj[key] = value;
+return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = Object.assign({}, obj);
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
